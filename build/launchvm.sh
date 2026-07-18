23c38e14dde9cbedf7c64182d14340814e55a5a4
y4my4my4m <8145020+y4my4my4m@users.noreply.github.com>
2026-07-18 19:50:43 +0900

    Add ZealAmp audio player and related applications
    - Introduced ZealAmp, a Winamp-style audio player for y4mOS, with support for 16-bit PCM WAV playback.
    - Added a calculator and calendar application to the desktop environment.
    - Implemented an AC'97 audio driver for improved sound functionality.
    - Updated the launcher menu to include new applications and enhanced settings management for audio volume control.
    - Added necessary image assets for ZealAmp's user interface.
    - Created a README file for the music directory to guide users on supported audio formats.

--- build/launchvm.sh
+++ build/launchvm.sh
@@ -5,6 +5,7 @@
 
 
 qemu-system-x86_64 -audiodev sdl,id=snd0 -machine q35,kernel_irqchip=on,pcspk-audiodev=snd0,accel=kvm \
+    -device AC97,audiodev=snd0 \
     -cdrom ZealOS-PublicDomain-BIOS-2026-07-17-22_04_05.iso \
     -drive file=ZealOS.qcow2,format=qcow2,if=ide \
     -m 2G -smp "$(nproc)" -rtc base=localtime -nic user,model=pcnet
